bd91de5da36b555430cbbff80830a41458e45c82
Devan Looches <devan.looches@gmail.com>
2021-07-02 16:04:00 -0700

    Added methods

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,13 @@
 use crate::modules::*;
+use user_error::{UFE, UserFacingError};
 
 #[derive(Deserialize)]
 #[serde(default)]
 #[serde(rename_all = "kebab-case")]
 #[serde(deny_unknown_fields)]
 pub struct Config {
+    module_order: String,
+    offset: u16,
     user: User,
 }
 
@@ -23,12 +26,32 @@ impl Config {
 
         toml::from_str::<Config>(&string).unwrap()
     }
+
+    fn module_order(&self) -> Vec<String> {
+        let mut vec = Vec::new();
+        for module in self.module_order.split_whitespace() {
+            match module {
+                "user" => vec.push(User::get_info()),
+                v => {
+                   UserFacingError::new("Failed to parse module order string.").reason(format!("Unknown module: {}", v)).print_and_exit();
+                   unreachable!();
+                }
+            }
+        }
+        vec
+    }
+
+    pub fn print_classic(&self) {
+        
+    }
 }
 
 impl Default for Config {
     fn default() -> Self {
         Config {
             user: User::default(),
+            offset: 4,
+            module_order: String::from("user"),
         }
     }
 }
